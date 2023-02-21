891fd2f6032d9658a699221dfa9bac2d16519c0b
zai-vik <118923977+zai-vik@users.noreply.github.com>
2023-02-21 10:12:59 +0300

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,17 @@
+const json_string = `{
+ "list": [
+  {
+   "name": "Petr",
+   "age": "20",
+   "prof": "mechanic"
+  },
+  {
+   "name": "Vova",
+   "age": "60",
+   "prof": "pilot"
+  }
+ ]
+}`;
+
+let result = JSON.parse(json_string);
+console.log(result);
